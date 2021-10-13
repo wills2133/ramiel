e8506415402bb61ea16de8d0dc03437a6921857e
Xiaojun Wu <xiaojunwu@microsoft.com>
2021-10-12 23:09:08 -0700

    7.76 update cart repository

--- Mango.Services.ShoppingCartAPI/DbContext/ApplicationDbContext.cs
+++ Mango.Services.ShoppingCartAPI/DbContext/ApplicationDbContext.cs
@@ -14,7 +14,7 @@ namespace Mango.Services.ShoppingCartAPI.DbContexts
 
         }
         public DbSet<Product> Products { get; set; }
-        public DbSet<CartHeader> CartHeader { get; set; }
+        public DbSet<CartHeader> CartHeaders { get; set; }
         public DbSet<CartDetails> CartDetails { get; set; }
 
     }
