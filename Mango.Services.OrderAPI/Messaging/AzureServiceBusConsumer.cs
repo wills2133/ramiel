3a85caaf367bfc321f7b653149a084c20d5cbde7
Xiaojun Wu <xiaojunwu@microsoft.com>
2021-10-17 14:19:20 -0700

    14.128 set up dependency injection to get message, load service id, topic, subscription

---
+++ Mango.Services.OrderAPI/Messaging/AzureServiceBusConsumer.cs
@@ -0,0 +1,75 @@
+﻿using Azure.Messaging.ServiceBus;
+using Mango.Services.OrderAPI.Messages;
+using Mango.Services.OrderAPI.Models;
+using Mango.Services.OrderAPI.Repository;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mango.Services.OrderAPI.Messaging
+{
+    public class AzureServiceBusConsumer
+    {
+        private readonly OrderRepository _orderRepository;
+        private readonly string servicBusConnectonString;
+        private readonly string subscriptonName;
+        private readonly string checkoutMessageTopic;
+
+        private readonly IConfiguration _configuration;
+
+        public AzureServiceBusConsumer(OrderRepository orderRepository, IConfiguration configuration)
+        {
+            _orderRepository = orderRepository;
+            _configuration = configuration;
+
+            servicBusConnectonString = _configuration.GetValue<string>("ServiceBusConnectionString");
+            subscriptonName = _configuration.GetValue<string>("SubscriptionName");
+            checkoutMessageTopic = _configuration.GetValue<string>("CheckoutMessageTopic");
+        }
+
+        private async Task OnCheckOutMessageReceived(ProcessMessageEventArgs args)
+        {
+            var message = args.Message;
+            var body = Encoding.UTF8.GetString(message.Body);
+
+            CheckoutHeaderDto checkoutHeaderDto = JsonConvert.DeserializeObject<CheckoutHeaderDto>(body);
+
+            OrderHeader orderHeader = new()
+            {
+                UserId = checkoutHeaderDto.UserId,
+                FirstName = checkoutHeaderDto.FirstName,
+                LastName = checkoutHeaderDto.LastName,
+                OrderDetails = new List<OrderDetails>(),
+                CardNumber = checkoutHeaderDto.CardNumber,
+                CouponCode = checkoutHeaderDto.CouponCode,
+                CVV = checkoutHeaderDto.CVV,
+                DiscountTotal = checkoutHeaderDto.DiscountTotal,
+                Email = checkoutHeaderDto.Email,
+                ExpiryMonthYear = checkoutHeaderDto.ExpiryMonthYear,
+                OrderTime = DateTime.Now,
+                OrderTotal = checkoutHeaderDto.OrderTotal,
+                PaymentStatus = false,
+                Phone = checkoutHeaderDto.Phone,
+                PickupDateTime = checkoutHeaderDto.PickupDateTime
+            };
+
+            foreach(var detailList in checkoutHeaderDto.CartDetails)
+            {
+                OrderDetails orderDetails = new()
+                {
+                    ProductId = detailList.ProductId,
+                    ProductName = detailList.Product.Name,
+                    Price = detailList.Product.Price,
+                    Count = detailList.Count
+                };
+                orderHeader.CartTotalItems += detailList.Count;
+                orderHeader.OrderDetails.Add(orderDetails);
+            }
+            await _orderRepository.AddOrder(orderHeader);
+        }
+    }
+}
